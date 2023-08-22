ae5d379b98b3b120c465b87535176618014ea03d
Carine Toledo <92695157+Carinetoledo@users.noreply.github.com>
2023-08-22 09:56:11 -0300

    ContaTerminal.java

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -1,4 +1,4 @@
-package desafio_dio_conta_bancaria;
+package desafio;
 
 import java.util.Scanner;
 
@@ -11,29 +11,35 @@ public class ContaTerminal {
 		String agencia;
 		String nomeCliente;
 		double saldo;
+		String sair;
 
-		System.out.println("---------------CONTA BANCÁRIA---------------");
+		System.out.println("--------CONTA BANCÁRIA----------");
 		System.out.println();
-		System.out.println("Por favor, digite o número da Conta: (1234)");
+		System.out.println("1 - Por favor, digite o número da Conta: (1234)");
 		numero = sc.nextInt();
 
-		System.out.println("Agora digite o número da Agência: (123-8)");
+		System.out.println("2 - Por favor, digite o número da Agência: (123-0)");
 		agencia = sc.next();
 
-		System.out.println("Me informe seu nome: ");
+		System.out.println("3 - Mostrar dados: ");
 		nomeCliente = sc.next();
 		sc.nextLine();
 
-		System.out.println("Agora me informa seu saldo atual: ");
+		System.out.println("4 - Mostrar saldo atual: ");
 		saldo = sc.nextDouble();
 
+  		System.out.println("0 - Sair -----");
+            	leitura.nextLine();
+
 		System.out.println();
-		System.out.printf("Olá %s, obrigado por criar uma conta em "
+		System.out.printf("Olá %s, obrigado por criar uma conta"
 				+ "nosso banco, sua agência é %s, "
 				+ "conta %d e seu saldo %.2f já está disponível"
-				+ " para saque.", nomeCliente, agencia, numero, saldo);
+				+ " para saque.", nomeCliente, agencia, numero, saldo, sair);
 
 		sc.close();
 	}
 
 }
+
+
